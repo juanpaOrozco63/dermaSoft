60cf572a5bc375fd25b065fb0e0df5f7731e32f7
Juan Pablo Orozco <juan-pablo01@hotmail.com>
2021-03-02 20:12:15 -0500

    menu lateral

--- src/app/admin/components/admin-principal/admin-principal.component.ts
+++ src/app/admin/components/admin-principal/admin-principal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-admin-principal',
@@ -10,6 +11,10 @@ export class AdminPrincipalComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $('#sidebarCollapse').on('click', function() {
+      $('#sidebar').toggleClass('active');
+      $(this).toggleClass('active');
+  });
   }
 
 }
